8780c1d0e02d14b351846626e5dc47d548a9bf03
yzw <963542298@qq.com>
2019-08-28 09:55:39 +0800

    订单详情service层更新

--- src/main/java/com/restaurant/service/IDetailsService.java
+++ src/main/java/com/restaurant/service/IDetailsService.java
@@ -1,6 +1,9 @@
 package com.restaurant.service;
 
+import com.restaurant.entity.Details;
+import com.restaurant.entity.Food;
 import com.restaurant.entity.Orders;
+import com.restaurant.entity.WrapperUtil;
 
 import java.util.List;
 
@@ -13,5 +16,12 @@ public interface IDetailsService {
      * 查询所有的订单
      * @return
      */
-    List<Orders> queryAllOrders();
+    List<Details> queryDetailsByOrdersId(Integer ordersid);
+
+    /**
+     * 插入订单详情
+     * @param wrapperUtil
+     * @return
+     */
+    int addDetails(WrapperUtil wrapperUtil);
 }
